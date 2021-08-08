473097520e6f3a57f992435325db9abe3e3712d3
Daniel Muñoz <info@danimm.com>
2021-08-08 22:27:29 +0200

    primevue and chartjs

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-import PrimeVue from 'primevue/config';
+import PrimeVue from "primevue/config";
+
+import "primevue/resources/themes/vela-green/theme.css"; //theme
+import "primevue/resources/primevue.min.css"; //core css
+import "primeicons/primeicons.css"; //icons
+
+import Button from "primevue/button";
 
 const app = createApp(App);
-
 app.use(store);
 app.use(router);
+
 app.use(PrimeVue);
-app.mount('#app');
+app.component("Button", Button);
+
+app.mount("#app");
