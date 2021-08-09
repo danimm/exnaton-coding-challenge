7e6ebe762b4908beddff25e0ca0e6ff179b75d05
Daniel Muñoz <info@danimm.com>
2021-08-09 20:54:17 +0200

    creating basic components on home

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,12 @@ import PrimeVue from "primevue/config";
 import "primevue/resources/themes/vela-green/theme.css"; //theme
 import "primevue/resources/primevue.min.css"; //core css
 import "primeicons/primeicons.css"; //icons
+import "primeflex/primeflex.css"; // flex.spaces,grid
 
 import Button from "primevue/button";
+import Toolbar from "primevue/toolbar";
+import Calendar from "primevue/calendar";
+import Chart from "primevue/chart";
 
 const app = createApp(App);
 app.use(store);
@@ -17,5 +21,8 @@ app.use(router);
 
 app.use(PrimeVue);
 app.component("Button", Button);
+app.component("Toolbar", Toolbar);
+app.component("Calendar", Calendar);
+app.component("Chart", Chart);
 
 app.mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <Button label="Hello" />
+  <div class="p-grid">
+    <toolbar>
+      <template #left>
+        <Button label="New" icon="pi pi-plus" class="p-mr-2" />
+        <Button label="Upload" icon="pi pi-upload" class="p-button-success" />
+        <Button label="Filter the results" class="p-button-primary p-ml-2" />
+      </template>
+      <template #right>
+        <Calendar :showWeek="true" />
+      </template>
+    </toolbar>
+  </div>
+  <div class="p-grid">
+    <div class="p-col">
+      <Chart type="bar" :data="basicData" height="150" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import { useStore } from "vuex";
 
 export default defineComponent({
@@ -14,7 +27,22 @@ export default defineComponent({
   setup() {
     const store = useStore();
     store.dispatch("getData");
-    return {};
+    const basicData = ref({
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+        {
+          label: "My First dataset",
+          backgroundColor: "#42A5F5",
+          data: [65, 59, 80, 81, 56, 55, 40],
+        },
+        {
+          label: "My Second dataset",
+          backgroundColor: "#9CCC65",
+          data: [28, 48, 40, 19, 86, 27, 90],
+        },
+      ],
+    });
+    return { basicData };
   },
 });
 </script>
