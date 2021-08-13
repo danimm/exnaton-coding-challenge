0eac4a879dbc66338bee29b2e574593b76aa8bfd
Daniel Muñoz <info@danimm.com>
2021-08-13 14:10:33 +0200

    working on right panel: options

---
+++ src/components/Home/Panel.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="panel-container">
+    <h3 class="p-text-white">Choose a range date to display</h3>
+    <div class="p-grid">
+      <div class="p-col">
+        <Calendar :showWeek="true" v-model="startDate" dateFormat="yy-mm-dd" />
+      </div>
+      <div class="p-col date-container">
+        <Calendar :showWeek="true" v-model="endDate" dateFormat="yy-mm-dd" />
+      </div>
+    </div>
+    <Button label="Search" style="width: 100%" />
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  name: "PanelComponent",
+  setup() {
+    const startDate = ref("");
+    const endDate = ref("");
+    return { startDate, endDate };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.panel-container {
+  background-color: #17212f;
+  height: 100vh;
+  padding: 20px;
+
+  .date-container {
+    margin-bottom: 20px;
+  }
+
+  h3 {
+    color: #eeeeee;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,77 +1,66 @@
 <template>
-  <div class="p-grid">
-    <toolbar>
-      <template #left>
-        <Button label="New" icon="pi pi-plus" class="p-mr-2" />
-        <Button label="Upload" icon="pi pi-upload" class="p-button-success" />
-        <Button label="Filter the results" class="p-button-primary p-ml-2" />
-      </template>
-      <template #right>
-        <Calendar :showWeek="true" />
-      </template>
-    </toolbar>
-  </div>
-  <div class="p-grid">
-    <div class="p-col-11 offset-1">
-      <Chart type="bar" :data="basicData" :options="options" :height="150" />
+  <div class="p-flex">
+    <div class="p-grid">
+      <div class="p-col-8">
+        <dashboard />
+      </div>
+      <div class="p-col-4">
+        <panel />
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import Dashboard from "@/components/Home/Dashboard.vue";
+import Panel from "@/components/Home/Panel.vue";
 import { defineComponent, ref, computed } from "vue";
 import { useStore } from "vuex";
 
 export default defineComponent({
+  components: { Dashboard, Panel },
   name: "Home",
   setup() {
     const store = useStore();
-    store.dispatch("getData");
-    const may = computed(() => store.state.may);
-    const june = computed(() => store.state.june);
-    const july = computed(() => store.state.july);
-    // const xAxis =
-    //   may.value.map((el: any) => el.x) +
-    //   june.value.map((el: any) => el.x) +
-    //   july.value.map((el: any) => el.x);
-
-    // console.log(xAxis);
+    const date = ref("");
 
     const basicData = ref({
       // labels: [xAxis],
       // labels: ["January", "February", "March", "April", "May", "June", "July"],
-      datasets: [
-        {
-          label: "May",
-          data: may.value,
-          backgroundColor: "#9CCC65",
-          // parsing: {
-          //   xAxisKey: "x",
-          // },
-        },
-        {
-          label: "June",
-          data: june.value,
-          backgroundColor: "#42A5F5",
-          // parsing: {
-          //   xAxisKey: "x",
-          // },
-        },
-        {
-          label: "July",
-          data: july.value,
-          backgroundColor: "#FFD371",
-          // parsing: {
-          //   xAxisKey: "x",
-          // },
-        },
-      ],
+      // datasets: [
+      //   {
+      //     label: "May",
+      //     data: may.value,
+      //     backgroundColor: "#9CCC65",
+      //     // parsing: {
+      //     //   xAxisKey: "x",
+      //     // },
+      //   },
+      //   {
+      //     label: "June",
+      //     data: june.value,
+      //     backgroundColor: "#42A5F5",
+      //     // parsing: {
+      //     //   xAxisKey: "x",
+      //     // },
+      //   },
+      //   {
+      //     label: "July",
+      //     data: july.value,
+      //     backgroundColor: "#FFD371",
+      //     // parsing: {
+      //     //   xAxisKey: "x",
+      //     // },
+      //   },
+      // ],
     });
 
     const options = ref({
       responsive: true,
     });
-    return { basicData, options };
+    return { basicData, options, date };
   },
 });
 </script>
+
+<style lang="scss" scoped></style>
