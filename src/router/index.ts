ef02ad775f12b9e89ee389f88bafdf4f483d6ff9
Daniel Muñoz <info@danimm.com>
2021-08-15 17:16:27 +0200

    404 page added

--- src/router/index.ts
+++ src/router/index.ts
@@ -20,6 +20,14 @@ const routes: Array<RouteRecordRaw> = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/Login.vue"),
   },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ "../views/404.vue"),
+  },
 ];
 
 const router = createRouter({
---
+++ src/views/404.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <exnaton-card class="card">
+      <template #title>
+        <h3>Error 404</h3>
+      </template>
+      <template #content>
+        <h3>Page not found</h3>
+        <h3>Try to go to <router-link to="/login">login page</router-link></h3>
+      </template>
+    </exnaton-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Error page",
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+
+  .card {
+    border-radius: 30px;
+    text-align: center;
+    padding: 50px;
+    width: 400px;
+
+    a {
+      color: white;
+    }
+  }
+}
+</style>
